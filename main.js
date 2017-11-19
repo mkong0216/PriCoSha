1586bb4c6bcfa2d3fc9640473a4eecb1f5fb1b93
Mandy Kong <mk5506@nyu.edu>
2017-11-18 20:47:17 -0500

    using ejs to render templates; error message if incorrect username or password

--- main.js
+++ main.js
@@ -18,15 +18,31 @@ connection.connect(function(err) {
 var http = require("http");
 var express = require("express");
 var app = express();
+app.set('view engine', 'ejs');  
+var bodyParser = require('body-parser');
+var md5 = require('md5');
 
 app.get('/', function(req, res) {
-	res.sendFile('templates/index.html', {root: __dirname });
+	var error = undefined;
+	res.render('index');
 });
 
-app.use(express.bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Attempting to login
 app.post('/login', function(req, res) {
-	console.log('Username: ' + req.body.username);
-	console.log('Password: ' + req.body.password);
-})
+	var username = req.body.username;
+	var password = md5(req.body.password);
+	var query = "SELECT * FROM Person WHERE username = ? AND password = ?";
+	connection.query(query, [username, password], function(err, rows, fields) {
+		if (rows.length === 0) {
+			error = "The username or password you entered is incorrect."
+			res.render('index', {error: error});
+		} else {
+			res.send('User exists!');
+		}
+	});
+});
 
 app.listen(3000, () => console.log("Server running at http://localhost:3000/"));
